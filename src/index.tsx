bfe3f867ad417a0b9481a6baa3ecb47af2f5e68e
Otay Nacef <46994974+OtayNacef@users.noreply.github.com>
2021-02-11 17:37:47 +0000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { render } from "react-dom";
+import AutoComplete from "./AutoComplete";
+
+const rootElement = document.getElementById("root");
+render(
+  <>
+    <h2>Auto Complete Using Simple React AutoComplete Tsx</h2>
+    <AutoComplete />
+  </>,
+  rootElement
+);
